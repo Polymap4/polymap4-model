8e6687b6f23ff401797af9edf70307b504ea2011
Falko Bräutigam <falko@polymap.de>
2018-01-28 16:43:28 +0100

    #145: [Import] WFS (http://github.com/Polymap4/polymap4-p4/issues/145)
    PropertyChangeSupport: implement CollectionPropertyConcern (for ILayer)

--- org.polymap.model2/src/org/polymap/model2/runtime/event/PropertyChangeSupport.java
+++ org.polymap.model2/src/org/polymap/model2/runtime/event/PropertyChangeSupport.java
@@ -1,6 +1,6 @@
 /* 
  * polymap.org
- * Copyright (C) 2012-2014, Falko Bräutigam. All rights reserved.
+ * Copyright (C) 2012-2018, Falko Bräutigam. All rights reserved.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
@@ -18,10 +18,10 @@ import java.util.Collection;
 
 import java.beans.PropertyChangeEvent;
 
-import org.apache.commons.logging.LogFactory;import org.apache.commons.logging.Log;
-
 import org.polymap.core.runtime.event.EventManager;
 
+import org.polymap.model2.CollectionProperty;
+import org.polymap.model2.CollectionPropertyConcern;
 import org.polymap.model2.Entity;
 import org.polymap.model2.ManyAssociation;
 import org.polymap.model2.Property;
@@ -32,17 +32,15 @@ import org.polymap.model2.runtime.ValueInitializer;
 
 /**
  * Fires {@link PropertyChangeEvent}s via {@link EventManager} when the value of a
- * property changes. Supports {@link Property} and {@link ManyAssociation}.
+ * property changes. Supports {@link Property}, {@link CollectionProperty} and
+ * {@link ManyAssociation}.
  * 
  * @author <a href="http://www.polymap.de">Falko Bräutigam</a>
  */
 public class PropertyChangeSupport
         extends PropertyConcernBase
-        implements PropertyConcern, ManyAssociation {
+        implements PropertyConcern, CollectionPropertyConcern, ManyAssociation {
 
-    private static final Log log = LogFactory.getLog( PropertyChangeSupport.class );
-
-    
     @Override
     public Object get() {
         return ((Property)delegate).get();
@@ -59,45 +57,55 @@ public class PropertyChangeSupport
         fireEvent( null, value );
     }
 
+    // Collection: ManyAssociation or CollectionProperty
+    
     @Override
     public boolean add( Object e ) {
-       boolean result = ((ManyAssociation)delegate).add( e );
+       boolean result = ((Collection)delegate).add( e );
        fireEvent( null, e );
        return result;
     }
 
     @Override
     public boolean remove( Object e ) {
-       boolean result = ((ManyAssociation)delegate).remove( e );
+       boolean result = ((Collection)delegate).remove( e );
        fireEvent( null, e );
        return result;
     }
 
     @Override
     public boolean addAll( Collection c ) {
-        boolean result = ((ManyAssociation)delegate).addAll( c );
+        boolean result = ((Collection)delegate).addAll( c );
         fireEvent( null, null );
         return result;
     }
 
     @Override
     public boolean removeAll( Collection c ) {
-        boolean result = ((ManyAssociation)delegate).removeAll( c );
+        boolean result = ((Collection)delegate).removeAll( c );
         fireEvent( null, null );
         return result;
     }
 
     @Override
     public boolean retainAll( Collection c ) {
-        boolean result = ((ManyAssociation)delegate).retainAll( c );
+        boolean result = ((Collection)delegate).retainAll( c );
         fireEvent( null, null );
         return result;
     }
 
     @Override
     public void clear() {
-        ((ManyAssociation)delegate).clear();
+        ((Collection)delegate).clear();
         fireEvent( null, null );
+    }
+    
+    /** {@link CollectionPropertyConcern} */
+    @Override
+    public Object createElement( ValueInitializer initializer ) {
+        Object result = ((CollectionProperty)delegate).createElement( initializer );
+        fireEvent( null, null );
+        return result;
     }
 
     protected void fireEvent( Object oldValue, Object newValue ) {
